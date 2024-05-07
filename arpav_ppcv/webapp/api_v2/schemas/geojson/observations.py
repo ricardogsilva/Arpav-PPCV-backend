096d566701b68a3e93f485f3cfd832b7e14e0d21
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2024-05-07 12:04:55 +0100

    Fix feature item's type and move id out of properties

--- arpav_ppcv/webapp/api_v2/schemas/geojson/observations.py
+++ arpav_ppcv/webapp/api_v2/schemas/geojson/observations.py
@@ -12,7 +12,8 @@ from .base import ArpavFeatureCollection
 class StationFeatureCollectionItem(geojson_pydantic.Feature):
     model_config = pydantic.ConfigDict(arbitrary_types_allowed=True)
 
-    type: str = "Point"
+    type: str = "Feature"
+    id: pydantic.UUID4
     geometry: fields.WkbElement
     links: list[str]
 
@@ -24,8 +25,14 @@ class StationFeatureCollectionItem(geojson_pydantic.Feature):
     ) -> "StationFeatureCollectionItem":
         url = request.url_for("get_station", **{"station_id": instance.id})
         return cls(
+            id=instance.id,
             geometry=instance.geom,
-            properties=instance.model_dump(exclude={"geom"}),
+            properties=instance.model_dump(
+                exclude={
+                    "id",
+                    "geom",
+                }
+            ),
             links=[str(url)]
         )
 
