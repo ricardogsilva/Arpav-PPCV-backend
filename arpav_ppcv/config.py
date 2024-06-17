6691afcbf746f9c1bbb472344b5edb29e294da4c
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2024-06-17 16:08:43 +0100

    Removed unused configuration keys

--- arpav_ppcv/config.py
+++ arpav_ppcv/config.py
@@ -1,5 +1,4 @@
 import logging
-import re
 from pathlib import Path
 from typing import Optional
 
@@ -15,56 +14,10 @@ class ContactSettings(pydantic.BaseModel):
     email: str = "info@geobeyond.it"
 
 
-class ThreddsDatasetSettings(pydantic.BaseModel):
-    thredds_url_pattern: str
-    unit: str | None = None
-    palette: str
-    range: list[float]
-    allowed_values: dict[str, list[str]] | None = None
-
-    @pydantic.model_validator(mode="after")
-    def strip_slashes_from_urls(self):
-        self.thredds_url_pattern = self.thredds_url_pattern.strip("/")
-        return self
-
-    @pydantic.computed_field()
-    @property
-    def dataset_id_pattern(self) -> str:
-        id_parts = ["{identifier}"]
-        for match_obj in re.finditer(r"(\{\w+\})", self.thredds_url_pattern):
-            id_parts.append(match_obj.group(1))
-        return "-".join(id_parts)
-
-    def get_dynamic_id_parameters(self, dataset_id: str) -> dict[str, str]:
-        pattern_parts = re.finditer(
-            r"\{(\w+)\}", self.dataset_id_pattern.partition("-")[-1]
-        )
-        id_parts = dataset_id.split("-")[1:]
-        result = {}
-        for index, pattern_match_obj in enumerate(pattern_parts):
-            id_part = id_parts[index]
-            name = pattern_match_obj.group(1)
-            result[name] = id_part
-        return result
-
-    def validate_dataset_id(self, dataset_id: str) -> dict[str, str]:
-        id_parameters = self.get_dynamic_id_parameters(dataset_id)
-        logger.debug(f"{id_parameters=}")
-        allowed = self.allowed_values or {}
-        for name, value in id_parameters.items():
-            if value not in allowed.get(name, []):
-                raise ValueError(
-                    f"Invalid dataset identifier: {name!r} cannot take the "
-                    f"value {value!r}"
-                )
-        return id_parameters
-
-
 class ThreddsServerSettings(pydantic.BaseModel):
     base_url: str = "http://localhost:8080/thredds"
     wms_service_url_fragment: str = "wms"
     netcdf_subset_service_url_fragment: str = "ncss/grid"  # noqa
-    datasets: dict[str, ThreddsDatasetSettings] = pydantic.Field(default_factory=dict)
     uncertainty_visualization_scale_range: tuple[float, float] = pydantic.Field(
         default=(0, 9)
     )
@@ -76,19 +29,6 @@ class ThreddsServerSettings(pydantic.BaseModel):
         self.netcdf_subset_service_url_fragment = (
             self.netcdf_subset_service_url_fragment.strip("/")
         )
-        return self
-
-    @pydantic.model_validator(mode="after")
-    def validate_dataset_config_ids(self):
-        illegal_strings = ("-",)
-        for ds_conf_id in self.datasets.keys():
-            for patt in illegal_strings:
-                if patt in ds_conf_id:
-                    raise ValueError(
-                        f"Invalid dataset identifier: {ds_conf_id!r} - these patterns "
-                        f"are not allowed to be part of a dataset "
-                        f"identifier: {', '.join(repr(p) for p in illegal_strings)}"
-                    )
         return self
 
 
