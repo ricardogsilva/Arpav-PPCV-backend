3aa3f316f9aa084e09cfd174098045e946b41c09
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2024-07-23 18:47:57 +0100

    Adding integration test case for coverage time series endpoint

--- tests/test_webapp_v2_routers_coverages.py
+++ tests/test_webapp_v2_routers_coverages.py
@@ -1,6 +1,11 @@
+import random
+from unittest import mock
+
 import httpx
 
 from arpav_ppcv.schemas import coverages
+
+random.seed(0)
 
 
 def test_coverage_configurations_list(
@@ -24,3 +29,26 @@ def test_coverage_identifiers_list(
         headers={"accept": "application/json"},
     )
     assert list_response.status_code == 200
+
+
+def test_get_time_series(
+    test_client_v2_app: httpx.Client,
+    sample_coverage_configurations: list[coverages.CoverageConfiguration],
+    sample_csv_data: str,
+):
+    cov_conf = random.choice(sample_coverage_configurations)
+    with mock.patch("arpav_ppcv.operations.ncss") as mock_ncss_module:
+        mock_query_dataset = mock.AsyncMock()
+        mock_query_dataset.return_value = sample_csv_data
+        mock_ncss_module.query_dataset.return_value = mock_query_dataset
+        series_response = test_client_v2_app.get(
+            test_client_v2_app.app.url_path_for(
+                "get_time_series", coverage_identifier=str(cov_conf.id)
+            ),
+            params={
+                "coords": "POINT(11.5469 44.9524)",
+                "include_observation_data": False,
+            },
+            headers={"accept": "application/json"},
+        )
+        assert series_response.status_code == 200
