320e2aa3979ab0e9a5dbb81e17d1161be4f4099b
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2024-08-28 16:06:32 +0100

    Added load tests to CI

--- tests/loadtests/locustfile.py
+++ tests/loadtests/locustfile.py
@@ -32,11 +32,11 @@ def _(environment, **kwargs):
     if environment.stats.total.fail_ratio > 0.01:
         logging.error("Test failed due to failure ratio > 1%")
         environment.process_exit_code = 1
-    elif environment.stats.total.avg_response_time > 200:
-        logging.error("Test failed due to average response time ratio > 300 ms")
+    elif environment.stats.total.avg_response_time > 1000:
+        logging.error("Test failed due to average response time ratio > 1000 ms")
         environment.process_exit_code = 1
-    elif environment.stats.total.get_response_time_percentile(0.95) > 1500:
-        logging.error("Test failed due to 95th percentile response time > 1500 ms")
+    elif environment.stats.total.get_response_time_percentile(0.95) > 2000:
+        logging.error("Test failed due to 95th percentile response time > 2000 ms")
         environment.process_exit_code = 1
     else:
         environment.process_exit_code = 0
