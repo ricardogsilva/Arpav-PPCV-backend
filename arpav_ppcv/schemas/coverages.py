07eee56ff5c4ecf7e7aa0d30ca430ccf106727e7
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2024-09-06 18:20:23 +0100

    Added method to retrieve cov conf's main_netcdf_dataset_name

--- arpav_ppcv/schemas/coverages.py
+++ arpav_ppcv/schemas/coverages.py
@@ -39,7 +39,6 @@ class ConfigurationParameterValue(sqlmodel.SQLModel, table=True):
     )
     id: uuid.UUID = sqlmodel.Field(default_factory=uuid.uuid4, primary_key=True)
     name: str
-    internal_value: str
     display_name_english: Optional[str] = None
     display_name_italian: Optional[str] = None
     description_english: Optional[str] = None
@@ -60,17 +59,7 @@ class ConfigurationParameterValue(sqlmodel.SQLModel, table=True):
 
 
 class ConfigurationParameterValueCreate(sqlmodel.SQLModel):
-    internal_value: str
-    name: Annotated[
-        str,
-        pydantic.Field(
-            pattern=_NAME_PATTERN,
-            help=(
-                "Parameter value name. Only alphanumeric characters and the underscore "
-                "are allowed. Example: my_param_value"
-            ),
-        ),
-    ] = None
+    name: str
     configuration_parameter_id: uuid.UUID
     display_name_english: Optional[str] = None
     display_name_italian: Optional[str] = None
@@ -99,17 +88,7 @@ class ConfigurationParameter(sqlmodel.SQLModel, table=True):
 class ConfigurationParameterValueCreateEmbeddedInConfigurationParameter(
     sqlmodel.SQLModel
 ):
-    internal_value: str
-    name: Annotated[
-        str,
-        pydantic.Field(
-            pattern=_NAME_PATTERN,
-            help=(
-                "Parameter value name. Only alphanumeric characters and the underscore "
-                "are allowed. Example: my_param_value"
-            ),
-        ),
-    ] = None
+    name: str
     display_name_english: Optional[str] = None
     display_name_italian: Optional[str] = None
     description_english: Optional[str] = None
@@ -141,8 +120,7 @@ class ConfigurationParameterValueUpdateEmbeddedInConfigurationParameterEdit(
     sqlmodel.SQLModel
 ):
     id: Optional[uuid.UUID] = None
-    internal_value: Optional[str] = None
-    name: Annotated[Optional[str], pydantic.Field(pattern=_NAME_PATTERN)] = None
+    name: Optional[str] = None
     display_name_english: Optional[str] = None
     display_name_italian: Optional[str] = None
     description_english: Optional[str] = None
@@ -278,37 +256,14 @@ class CoverageConfiguration(sqlmodel.SQLModel, table=True):
         all_parts = ["name"] + sorted(list(other_parts))
         return "-".join(f"{{{part}}}" for part in all_parts)
 
-    @pydantic.computed_field()
-    @property
-    def archive(self) -> Optional[str]:
-        result = None
-        for pv in self.possible_values:
-            if (
-                pv.configuration_parameter_value.configuration_parameter.name
-                == "archive"
-            ):
-                result = pv.configuration_parameter_value.name
-                break
-        return result
-
     def get_thredds_url_fragment(self, coverage_identifier: str) -> str:
         return self._render_templated_value(
             coverage_identifier, self.thredds_url_pattern
         )
 
-    def get_main_netcdf_variable_name(self, coverage_identifier: str) -> str:
+    def get_main_netcdf_dataset_name(self, coverage_identifier: str) -> str:
         return self._render_templated_value(
             coverage_identifier, self.netcdf_main_dataset_name
-        )
-
-    def get_wms_main_layer_name(self, coverage_identifier: str) -> str:
-        return self._render_templated_value(
-            coverage_identifier, self.wms_main_layer_name
-        )
-
-    def get_wms_secondary_layer_name(self, coverage_identifier: str) -> str:
-        return self._render_templated_value(
-            coverage_identifier, self.wms_secondary_layer_name
         )
 
     def _render_templated_value(self, coverage_identifier: str, template: str) -> str:
@@ -322,10 +277,9 @@ class CoverageConfiguration(sqlmodel.SQLModel, table=True):
             param_name = (
                 used_value.configuration_parameter_value.configuration_parameter.name
             )
-            param_internal_value = (
-                used_value.configuration_parameter_value.internal_value
+            rendered = rendered.replace(
+                f"{{{param_name}}}", used_value.configuration_parameter_value.name
             )
-            rendered = rendered.replace(f"{{{param_name}}}", param_internal_value)
         return rendered
 
     def build_coverage_identifier(
@@ -385,27 +339,24 @@ class CoverageConfiguration(sqlmodel.SQLModel, table=True):
         self, coverage_identifier: str
     ) -> Optional[base.Season]:
         used_values = self.retrieve_used_values(coverage_identifier)
-        result = None
         for used_value in used_values:
             is_temporal_aggregation = (
                 used_value.configuration_parameter_value.configuration_parameter.name
-                in (
-                    "year_period",
-                    "historical_year_period",
-                )
+                in ("year_period",)
             )
             if is_temporal_aggregation:
                 value = used_value.configuration_parameter_value.name.lower()
-                if value in ("winter",):
+                if value in ("djf",):
                     result = base.Season.WINTER
-                elif value in ("spring",):
+                elif value in ("mam",):
                     result = base.Season.SPRING
-                elif value in ("summer",):
+                elif value in ("jja",):
                     result = base.Season.SUMMER
-                elif value in ("autumn",):
+                elif value in ("son",):
                     result = base.Season.AUTUMN
                 break
         else:
+            result = None
             logger.warning(
                 f"Could not determine appropriate season for coverage "
                 f"identifier {coverage_identifier!r}"
@@ -587,7 +538,7 @@ class CoverageInternal:
         return hash(self.identifier)
 
 
-class VariableMenuTreeCombination(TypedDict):
+class ForecastVariableMenuTreeCombination(TypedDict):
     configuration_parameter: ConfigurationParameter
     values: list[ConfigurationParameterValue]
 
@@ -596,10 +547,4 @@ class ForecastVariableMenuTree(TypedDict):
     climatological_variable: ConfigurationParameterValue
     aggregation_period: ConfigurationParameterValue
     measure: ConfigurationParameterValue
-    combinations: dict[str, VariableMenuTreeCombination]
-
-
-class HistoricalVariableMenuTree(TypedDict):
-    historical_variable: ConfigurationParameterValue
-    aggregation_period: ConfigurationParameterValue
-    combinations: dict[str, VariableMenuTreeCombination]
+    combinations: dict[str, ForecastVariableMenuTreeCombination]
