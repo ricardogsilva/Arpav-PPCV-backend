a3defcaad4581ae96ea3356bb024375b590b6b43
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2024-09-10 16:52:11 +0100

    Added bootstrap conf param for historical datasets

--- arpav_ppcv/bootstrapper/coverage_configurations/historical/tdd.py
+++ arpav_ppcv/bootstrapper/coverage_configurations/historical/tdd.py
@@ -1,3 +1,4 @@
+from ....schemas.base import ObservationAggregationType
 from ....schemas.coverages import (
     CoverageConfigurationCreate,
     ConfigurationParameterPossibleValueCreate,
@@ -7,34 +8,81 @@ _DISPLAY_NAME_ENGLISH = "Average temperature"
 _DISPLAY_NAME_ITALIAN = "Temperatura media"
 _DESCRIPTION_ENGLISH = "Average of average temperatures"
 _DESCRIPTION_ITALIAN = "Media delle temperature medie"
+_HISTORICAL_COLLECTION = "historical"
+_OBSERVATION_VARIABLE = "tdd"
+_UNIT = "ºC"
+_COLOR_SCALE_MIN = -5
+_COLOR_SCALE_MAX = 20
+_RELATED_OBSERVATION_VARIABLE_NAME = "TDd"
 
 
 def generate_configurations(
     conf_param_values, variables
 ) -> list[CoverageConfigurationCreate]:
-    return [
+    cov_confs = [
         CoverageConfigurationCreate(
-            name="tdd_30yr",
+            name="tdd_30yr_yearly",
             display_name_english=_DISPLAY_NAME_ENGLISH,
             display_name_italian=_DISPLAY_NAME_ITALIAN,
             description_english=_DESCRIPTION_ENGLISH,
             description_italian=_DESCRIPTION_ITALIAN,
-            netcdf_main_dataset_name="tas",
-            wms_main_layer_name="tas",
+            netcdf_main_dataset_name="{observation_year_period}_avg",
+            wms_main_layer_name="{observation_year_period}_avg",
             thredds_url_pattern="cline_30yr/TDd_1991-2020.nc",
-            unit="ºC",
+            unit=_UNIT,
             palette="default/seq-YlOrRd",
-            color_scale_min=0,
-            color_scale_max=6,
+            color_scale_min=_COLOR_SCALE_MIN,
+            color_scale_max=_COLOR_SCALE_MAX,
             possible_values=[
                 ConfigurationParameterPossibleValueCreate(
                     configuration_parameter_value_id=conf_param_values[
-                        ("collection", "forecast")
+                        ("collection", _HISTORICAL_COLLECTION)
                     ].id
                 ),
                 ConfigurationParameterPossibleValueCreate(
                     configuration_parameter_value_id=conf_param_values[
-                        ("climatological_variable", "tas")
+                        ("observation_variable", _OBSERVATION_VARIABLE)
+                    ].id
+                ),
+                ConfigurationParameterPossibleValueCreate(
+                    configuration_parameter_value_id=conf_param_values[
+                        ("aggregation_period", "30yr")
+                    ].id
+                ),
+                ConfigurationParameterPossibleValueCreate(
+                    configuration_parameter_value_id=conf_param_values[
+                        ("measure", "absolute")
+                    ].id
+                ),
+                ConfigurationParameterPossibleValueCreate(
+                    configuration_parameter_value_id=conf_param_values[
+                        ("observation_year_period", "A00")
+                    ].id
+                ),
+            ],
+        ),
+        CoverageConfigurationCreate(
+            name="tdd_annual_yearly",
+            display_name_english=_DISPLAY_NAME_ENGLISH,
+            display_name_italian=_DISPLAY_NAME_ITALIAN,
+            description_english=_DESCRIPTION_ENGLISH,
+            description_italian=_DESCRIPTION_ITALIAN,
+            netcdf_main_dataset_name="TDd",
+            wms_main_layer_name="TDd",
+            thredds_url_pattern="cline_yr/TDd_{observation_year_period}_1992-2023_py85.nc",
+            unit=_UNIT,
+            palette="default/seq-YlOrRd",
+            color_scale_min=_COLOR_SCALE_MIN,
+            color_scale_max=_COLOR_SCALE_MAX,
+            possible_values=[
+                ConfigurationParameterPossibleValueCreate(
+                    configuration_parameter_value_id=conf_param_values[
+                        ("collection", _HISTORICAL_COLLECTION)
+                    ].id
+                ),
+                ConfigurationParameterPossibleValueCreate(
+                    configuration_parameter_value_id=conf_param_values[
+                        ("observation_variable", _OBSERVATION_VARIABLE)
                     ].id
                 ),
                 ConfigurationParameterPossibleValueCreate(
@@ -44,49 +92,231 @@ def generate_configurations(
                 ),
                 ConfigurationParameterPossibleValueCreate(
                     configuration_parameter_value_id=conf_param_values[
-                        ("measure", "anomaly")
+                        ("measure", "absolute")
                     ].id
                 ),
                 ConfigurationParameterPossibleValueCreate(
                     configuration_parameter_value_id=conf_param_values[
-                        ("climatological_model", "model_ensemble")
-                    ].id
-                ),
-                ConfigurationParameterPossibleValueCreate(
-                    configuration_parameter_value_id=conf_param_values[
-                        ("scenario", "rcp26")
-                    ].id
-                ),
-                ConfigurationParameterPossibleValueCreate(
-                    configuration_parameter_value_id=conf_param_values[
-                        ("scenario", "rcp45")
-                    ].id
-                ),
-                ConfigurationParameterPossibleValueCreate(
-                    configuration_parameter_value_id=conf_param_values[
-                        ("scenario", "rcp85")
-                    ].id
-                ),
-                ConfigurationParameterPossibleValueCreate(
-                    configuration_parameter_value_id=conf_param_values[
-                        ("year_period", "DJF")
-                    ].id
-                ),
-                ConfigurationParameterPossibleValueCreate(
-                    configuration_parameter_value_id=conf_param_values[
-                        ("year_period", "MAM")
-                    ].id
-                ),
-                ConfigurationParameterPossibleValueCreate(
-                    configuration_parameter_value_id=conf_param_values[
-                        ("year_period", "JJA")
-                    ].id
-                ),
-                ConfigurationParameterPossibleValueCreate(
-                    configuration_parameter_value_id=conf_param_values[
-                        ("year_period", "SON")
+                        ("observation_year_period", "A00")
                     ].id
                 ),
             ],
+            observation_variable_id=(
+                v.id
+                if (v := variables.get(_RELATED_OBSERVATION_VARIABLE_NAME)) is not None
+                else None
+            ),
+            observation_variable_aggregation_type=ObservationAggregationType.YEARLY,
         ),
     ]
+    for season_name, season_id, duration in (
+        ("winter", "S01", "1992-2024"),
+        ("spring", "S02", "1992-2024"),
+        ("summer", "S03", "1992-2023"),
+        ("autumn", "S04", "1992-2023"),
+    ):
+        cov_confs.extend(
+            [
+                CoverageConfigurationCreate(
+                    name=f"tdd_30yr_{season_name}",
+                    display_name_english=_DISPLAY_NAME_ENGLISH,
+                    display_name_italian=_DISPLAY_NAME_ITALIAN,
+                    description_english=_DESCRIPTION_ENGLISH,
+                    description_italian=_DESCRIPTION_ITALIAN,
+                    netcdf_main_dataset_name="{observation_year_period}_avg",
+                    wms_main_layer_name="{observation_year_period}_avg",
+                    thredds_url_pattern="cline_30yr/TDd_1991-2020.nc",
+                    unit=_UNIT,
+                    palette="default/seq-YlOrRd",
+                    color_scale_min=_COLOR_SCALE_MIN,
+                    color_scale_max=_COLOR_SCALE_MAX,
+                    possible_values=[
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("collection", _HISTORICAL_COLLECTION)
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("observation_variable", _OBSERVATION_VARIABLE)
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("aggregation_period", "30yr")
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("measure", "absolute")
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("observation_year_period", season_id)
+                            ].id
+                        ),
+                    ],
+                ),
+                CoverageConfigurationCreate(
+                    name=f"tdd_annual_{season_name}",
+                    display_name_english=_DISPLAY_NAME_ENGLISH,
+                    display_name_italian=_DISPLAY_NAME_ITALIAN,
+                    description_english=_DESCRIPTION_ENGLISH,
+                    description_italian=_DESCRIPTION_ITALIAN,
+                    netcdf_main_dataset_name="TDd",
+                    wms_main_layer_name="TDd",
+                    thredds_url_pattern=(
+                        f"cline_yr/TDd_{{observation_year_period}}_{duration}_py85.nc"
+                    ),
+                    unit=_UNIT,
+                    palette="default/seq-YlOrRd",
+                    color_scale_min=_COLOR_SCALE_MIN,
+                    color_scale_max=_COLOR_SCALE_MAX,
+                    possible_values=[
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("collection", _HISTORICAL_COLLECTION)
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("observation_variable", _OBSERVATION_VARIABLE)
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("aggregation_period", "annual")
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("measure", "absolute")
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("observation_year_period", season_id)
+                            ].id
+                        ),
+                    ],
+                    observation_variable_id=(
+                        v.id
+                        if (v := variables.get(_RELATED_OBSERVATION_VARIABLE_NAME))
+                        is not None
+                        else None
+                    ),
+                    observation_variable_aggregation_type=ObservationAggregationType.SEASONAL,
+                ),
+            ]
+        )
+    for month_name, month_id, duration in (
+        ("january", "M01", "1992-2024"),
+        ("february", "M02", "1992-2024"),
+        ("march", "M03", "1992-2024"),
+        ("april", "M04", "1992-2024"),
+        ("may", "M05", "1992-2024"),
+        ("june", "M06", "1992-2024"),
+        ("july", "M07", "1992-2024"),
+        ("august", "M08", "1992-2023"),
+        ("september", "M09", "1992-2023"),
+        ("october", "M10", "1992-2023"),
+        ("november", "M11", "1992-2023"),
+        ("december", "M12", "1992-2023"),
+    ):
+        cov_confs.extend(
+            [
+                CoverageConfigurationCreate(
+                    name=f"tdd_30yr_{month_name}",
+                    display_name_english=_DISPLAY_NAME_ENGLISH,
+                    display_name_italian=_DISPLAY_NAME_ITALIAN,
+                    description_english=_DESCRIPTION_ENGLISH,
+                    description_italian=_DESCRIPTION_ITALIAN,
+                    netcdf_main_dataset_name="{observation_year_period}_avg",
+                    wms_main_layer_name="{observation_year_period}_avg",
+                    thredds_url_pattern="cline_30yr/TDd_1991-2020.nc",
+                    unit=_UNIT,
+                    palette="default/seq-YlOrRd",
+                    color_scale_min=_COLOR_SCALE_MIN,
+                    color_scale_max=_COLOR_SCALE_MAX,
+                    possible_values=[
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("collection", _HISTORICAL_COLLECTION)
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("observation_variable", _OBSERVATION_VARIABLE)
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("aggregation_period", "30yr")
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("measure", "absolute")
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("observation_year_period", month_id)
+                            ].id
+                        ),
+                    ],
+                ),
+                CoverageConfigurationCreate(
+                    name=f"tdd_annual_{month_name}",
+                    display_name_english=_DISPLAY_NAME_ENGLISH,
+                    display_name_italian=_DISPLAY_NAME_ITALIAN,
+                    description_english=_DESCRIPTION_ENGLISH,
+                    description_italian=_DESCRIPTION_ITALIAN,
+                    netcdf_main_dataset_name="TDd",
+                    wms_main_layer_name="TDd",
+                    thredds_url_pattern=(
+                        f"cline_yr/TDd_{{observation_year_period}}_{duration}_py85.nc"
+                    ),
+                    unit=_UNIT,
+                    palette="default/seq-YlOrRd",
+                    color_scale_min=_COLOR_SCALE_MIN,
+                    color_scale_max=_COLOR_SCALE_MAX,
+                    possible_values=[
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("collection", _HISTORICAL_COLLECTION)
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("observation_variable", _OBSERVATION_VARIABLE)
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("aggregation_period", "annual")
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("measure", "absolute")
+                            ].id
+                        ),
+                        ConfigurationParameterPossibleValueCreate(
+                            configuration_parameter_value_id=conf_param_values[
+                                ("observation_year_period", month_id)
+                            ].id
+                        ),
+                    ],
+                    observation_variable_id=(
+                        v.id
+                        if (v := variables.get(_RELATED_OBSERVATION_VARIABLE_NAME))
+                        is not None
+                        else None
+                    ),
+                    observation_variable_aggregation_type=ObservationAggregationType.MONTHLY,
+                ),
+            ]
+        )
+    return cov_confs
