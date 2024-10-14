029e3fbb41511bb9c96e6d87a0c58e4fb0e5c4ee
Ricardo Garcia Silva <ricardo.garcia.silva@geobeyond.it>
2024-10-14 12:23:13 +0100

    Implement a way to sort a configuration parameters possible values manually - hotfix (#274)
    * Added sorting information to configuration parameter values
    
    Also sorted the combinations for forecast variables
    
    * Added sorting information to configuration parameter values
    
    Also sorted the combinations for historical variables
    
    * Fixed error with combination sorting

--- arpav_ppcv/webapp/api_v2/routers/coverages.py
+++ arpav_ppcv/webapp/api_v2/routers/coverages.py
@@ -754,7 +754,7 @@ def get_forecast_variable_combinations(
                 coverage_schemas.ForecastVariableCombinations.from_items(var_menu),
             )
         )
-    var_combinations.sort(key=itemgetter(0, 1, 2, 3))
+    var_combinations.sort(key=itemgetter(0, 1, 2))
     var_combinations = [vc[3] for vc in var_combinations]
     return coverage_schemas.ForecastVariableCombinationsList(
         combinations=var_combinations,
@@ -787,7 +787,7 @@ def get_historical_variable_combinations(
                 coverage_schemas.HistoricalVariableCombinations.from_items(var_menu),
             )
         )
-    var_combinations.sort(key=itemgetter(0, 1, 2))
+    var_combinations.sort(key=itemgetter(0, 1))
     var_combinations = [vc[2] for vc in var_combinations]
     return coverage_schemas.HistoricalVariableCombinationsList(
         combinations=var_combinations,
